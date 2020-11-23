b0365d0239edcf780e0f339dec4b730f375871a2
Kamil <s.kamilx@gmail.com>
2020-11-23 19:35:41 +0100

    Stage 2 completed

--- JSON Database/task/src/client/Main.java
+++ JSON Database/task/src/client/Main.java
@@ -3,11 +3,37 @@ package client;
 import server.ClientInterface;
 import server.DatabaseDao;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.Socket;
 import java.util.Scanner;
 
 public class Main {
 
+    private static final String SERVER_ADDRESS = "127.0.0.1";
+    private static final int SERVER_PORT = 34522;
+
     public static void main(String[] args) {
+        try (
+                Socket socket = new Socket(InetAddress.getByName(SERVER_ADDRESS), SERVER_PORT);
+                DataInputStream inputStream = new DataInputStream(socket.getInputStream());
+                DataOutputStream outputStream = new DataOutputStream(socket.getOutputStream());
+                ) {
+
+            System.out.println("Client started!");
+            String message = "Give me a record # 12";
+
+            outputStream.writeUTF(message);
+            System.out.println("Sent: " + message);
+            String recievedMessage = inputStream.readUTF();
+
+            System.out.println("Received: " + recievedMessage);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 }
--- JSON Database/task/src/server/Main.java
+++ JSON Database/task/src/server/Main.java
@@ -4,25 +4,33 @@ package server;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Main {
 
-    private static int PORT = 34500;
+    private static int PORT = 34522;
+    private static String address = "127.0.0.1";
 
     public static void main(String[] args) {
-        try (ServerSocket server = new ServerSocket(PORT)) {
-            while (true) {
+
+        try (ServerSocket server = new ServerSocket(PORT,50, InetAddress.getByName(address))) {
+            System.out.println("Server started!");
                 try (
                         Socket socket = server.accept();
                         DataInputStream inputStream = new DataInputStream(socket.getInputStream());
                         DataOutputStream outputStream = new DataOutputStream(socket.getOutputStream());
                         ) {
                     String message = inputStream.readUTF();
-                    outputStream.writeUTF(message);
+                    System.out.println("Recieved: " + message);
+                    String [] messageArray = message.split(" ");
+                    String outputMessage = "A record # 12 was sent!";
+
+                    outputStream.writeUTF(outputMessage);
+                    System.out.print("Sent: " + outputMessage);
                 }
-            }
+
 
         } catch (IOException e) {
             e.printStackTrace();
